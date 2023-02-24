7c7b822740ee81fc46bdeff695ff99b4bfd12da1
Wichapon V <wichapon@gmail.com>
2023-02-24 09:42:45 +0700

    Add code to calculate time until next quarter hour and check if at least 1 hour has elapsed since last alert before sending Line API message

--- BB_Line_API_V3 .py
+++ BB_Line_API_V3 .py
@@ -6,6 +6,10 @@ import time
 from binance.client import Client
 from dotenv import load_dotenv
 import os
+import datetime
+
+# set up initial timer value
+last_alert_time = 0
 
 load_dotenv()
 
@@ -40,25 +44,47 @@ i=0
 while True:
     try:
 
+        # get current minute value
+        current_time = datetime.datetime.now()
+        current_minute = current_time.minute
+
+        # calculate time until next quarter hour
+        if current_minute < 15:
+            wait_time = (15 - current_minute) * 60 - current_time.second
+        elif current_minute < 30:
+            wait_time = (30 - current_minute) * 60 - current_time.second
+        elif current_minute < 45:
+            wait_time = (45 - current_minute) * 60 - current_time.second
+        else:
+            wait_time = (60 - current_minute) * 60 - current_time.second
+
+        # wait until next quarter hour
+        time.sleep(wait_time)
+
+        # update data
         candle_data = client.futures_klines(symbol=symbol, interval=timeframe, limit=bb_period+1)
         low_prices = np.array([float(x[4]) for x in candle_data])
         upper_bb, middle_bb, lower_bb = talib.BBANDS(low_prices, timeperiod=bb_period, nbdevup=bb_stddev, nbdevdn=bb_stddev, matype=0)
 
         # check if the Bollinger Bands are contracting
         if upper_bb[-1] - lower_bb[-1] < middle_bb[-1]:
-            # check if the current candle hits the upper bound Bollinger Band line
+            # check if the current candle hits the upper middle Bollinger Band line
             if low_prices[-1] > middle_bb[-1]:
-                # send the alert via Line API
-                send_line_alert(line_token, f"{symbol}, price: {low_prices[-1]}, candle tf 15m hit upper bound Bollinger Band line on 1 hr")
+                # check if at least 1 hour has elapsed since the last alert
+                if time.time() - last_alert_time >= 3600:
+                    # send the alert via Line API
+                    send_line_alert(line_token, f"{symbol}, price: {low_prices[-1]}, candle tf 15m hit upper bound Bollinger Band line on 1 hr")
+                    # update the last alert time
+                    last_alert_time = time.time()
 
             # check if the current candle hits the lower bound Bollinger Band line
             if low_prices[-1] < lower_bb[-1]:
-                # send the alert via Line API
-                send_line_alert(line_token, f"{symbol}, price: {low_prices[-1]}, candle tf 15m hit lower bound Bollinger Band line on tf 1hr")
-    
-        # wait for the next candle
-        time.sleep(60*15) # wait for 15 minutes
-        
+                # check if at least 1 hour has elapsed since the last alert
+                if time.time() - last_alert_time >= 3600:
+                    # send the alert via Line API
+                    send_line_alert(line_token, f"{symbol}, price: {low_prices[-1]}, candle tf 15m hit lower bound Bollinger Band line on tf 1hr")
+                    # update the last alert time
+                    last_alert_time = time.time()
 
     except:
         # handle any errors
