a6fcba953b890cf6982f5ab273aee561c3ff1173
Wichapon V <wichapon@gmail.com>
2023-02-24 16:28:33 +0700

    Add logic to calculate time until next quarter hour in wait_duration.py

--- wait_duration.py
+++ wait_duration.py
@@ -8,3 +8,13 @@ if current_minute < 60:
     wait_time = ((60 - current_minute) * 60 - current_time.second)+5  #add 5 sec delay prevent .json data is not ready in time
     print(f'next notify in {wait_time/60} minutes')
     
+
+# calculate time until next quarter hour
+#if current_minute < 15:
+#    wait_time = (15 - current_minute) * 60 - current_time.second
+#elif current_minute < 30:
+#   wait_time = (30 - current_minute) * 60 - current_time.second
+#elif current_minute < 45:
+#    wait_time = (45 - current_minute) * 60 - current_time.second
+#else:
+#    wait_time = (60 - current_minute) * 60 - current_time.second 
