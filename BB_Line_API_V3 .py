abd335b29025a645483c78bf406da2019f3c6496
Wichapon V <wichapon@gmail.com>
2023-03-06 12:57:29 +0700

    Refactor code to initialize timer and flag variables

--- BB_Line_API_V3 .py
+++ BB_Line_API_V3 .py
@@ -7,70 +7,50 @@ from candle_data import candleData
 load_dotenv()
 
 # set up initial timer value and flag variables
-last_alert_time = 0
-upper_alert_triggered = False
-upper_middle_alert_triggered = False
-lower_middle_alert_triggered = False
-lower_alert_triggered = False
+last_alert_time, upper_alert_triggered, upper_middle_alert_triggered, lower_middle_alert_triggered, lower_alert_triggered = 0, False, False, False, False
 
 while True:
     try:
         wait_time = waitTime()  #get the value from def waitTime
         print(f'next notify in {wait_time/60} minutes')
         time.sleep(wait_time)
-
         # update data
         upper_bb, middle_bb, lower_bb,low_prices,high_prices = candleData()
 
         # check if the Bollinger Bands are contracting
         if upper_bb[-1] - lower_bb[-1] < middle_bb[-1]:
 
-            # check if the current highest price candle hits the middle Bollinger Band line
             if high_prices[-1] > middle_bb[-1]:
-                # check if the upper alert has not been triggered yet
                 if not upper_middle_alert_triggered:
                     send_line_alert(f"{symbol}, High candle price: {high_prices[-1]}, timeframe: {timeframe}, hit middle BB")
-                    # set the upper alert triggered flag
                     upper_middle_alert_triggered = True
             else:
                 # reset the upper alert triggered flag
                 upper_middle_alert_triggered = False
             
-
-            # check if the current lowest price candle hits the middle Bollinger Band line
             if low_prices[-1] < middle_bb[-1]:
-                # check if the upper alert has not been triggered yet
                 if not lower_middle_alert_triggered:
                     send_line_alert( f"{symbol}, Low candle price: {low_prices[-1]}, timeframe: {timeframe}, hit middle BB")
-                    # set the upper alert triggered flag
                     lower_middle_alert_triggered = True
             else:
                 # reset the upper alert triggered flag
                 lower_middle_alert_triggered = False
 
-            # check if the current candle hits the lower bound Bollinger Band line
-            if low_prices[-1] < lower_bb[-1]:
-                
+            if low_prices[-1] < lower_bb[-1]:               
                 if not lower_alert_triggered:
                     send_line_alert( f"{symbol}, Lowest price: {low_prices[-1]}, timeframe: {timeframe}, hit lower BB.")
-                    # set the upper alert triggered flag
                     lower_alert_triggered = True
             else:
                 # reset the upper alert triggered flag
                 lower_alert_triggered = False
 
-             # check if the current candle hits the upper bound Bollinger Band line
-            if high_prices[-1] > upper_bb[-1]:
-                
+            if high_prices[-1] > upper_bb[-1]:                
                 if not upper_alert_triggered:                  
                     send_line_alert( f"{symbol}, Lowest price: {high_prices[-1]}, timeframe: {timeframe}, hit lower BB.")
-                    # set the upper alert triggered flag
                     upper_alert_triggered = True
             else:
                 # reset the upper alert triggered flag
                 upper_alert_triggered = False
-
-
 
     except:
         # handle any errors
