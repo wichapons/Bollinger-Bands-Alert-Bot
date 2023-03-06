1e2cd32d0851421cc8ab5935d80c4cad5dfa6919
Wichapon V <wichapon@gmail.com>
2023-03-06 12:18:02 +0700

    Move config variables to config.py

--- BB_Line_API_V3 .py
+++ BB_Line_API_V3 .py
@@ -5,7 +5,7 @@ import json
 import time
 from binance.client import Client
 from dotenv import load_dotenv
-import os
+from config import *
 import datetime
 load_dotenv()
 
@@ -15,24 +15,6 @@ upper_alert_triggered = False
 upper_middle_alert_triggered = False
 lower_middle_alert_triggered = False
 lower_alert_triggered = False
-
-# set the Binance API key and secret
-binance_api_key = os.getenv('API_KEY')
-binance_api_secret = os.getenv('API_SECRET')
-
-# set the Line API access token
-line_token = os.getenv('LINE_TOKEN')
-
-# set the symbol and timeframe
-symbol = "BNBUSDT"
-timeframe = "1h"
-
-# set the Bollinger Band parameters
-bb_period = 20
-bb_stddev = 2
-
-# create a Binance client
-client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
 
 # function to send a Line API alert
 def send_line_alert(line_token, message):
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from binance.client import Client
 from dotenv import load_dotenv
 load_dotenv()
 
+
 # set the Binance API key and secret
 binance_api_key = os.getenv('API_KEY')
 binance_api_secret = os.getenv('API_SECRET')
@@ -10,13 +11,13 @@ binance_api_secret = os.getenv('API_SECRET')
 # set the Line API access token
 line_token = os.getenv('LINE_TOKEN')
 
+# set the symbol and timeframe
+symbol = "BNBUSDT"
+timeframe = "1h"
+
 # set the Bollinger Band parameters
 bb_period = 20
 bb_stddev = 2
 
 # create a Binance client
 client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
-
-# set the symbol and timeframe
-symbol = "BNBUSDT"
-timeframe = "1h"
