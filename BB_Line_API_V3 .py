dc39f7bf6966e8f5b55ed24d8feae97db9d6cb95
Wichapon V <wichapon@gmail.com>
2023-03-11 12:19:48 +0700

    remove alert when price hit middle bb

--- BB_Line_API_V3 .py
+++ BB_Line_API_V3 .py
@@ -24,22 +24,22 @@ while True:
 
         # check if the Bollinger Bands are contracting
         if upper_bb[-1] - lower_bb[-1] < middle_bb[-1]:
-
-            if high_prices[-1] > middle_bb[-1]:
-                if not upper_middle_alert_triggered:
-                    send_line_alert(f"{symbol}, Highest candle price: {high_prices[-1]}, timeframe: {timeframe}, hit middle BB")
-                    upper_middle_alert_triggered = True
-            else:
-                # reset the upper alert triggered flag
-                upper_middle_alert_triggered = False
+#
+#            if high_prices[-1] > middle_bb[-1]:
+#                if not upper_middle_alert_triggered:
+#                    send_line_alert(f"{symbol}, Highest candle price: {high_prices[-1]}, timeframe: {timeframe}, hit middle BB")
+#                    upper_middle_alert_triggered = True
+#            else:
+#                # reset the upper alert triggered flag
+#                upper_middle_alert_triggered = False
             
-            if low_prices[-1] < middle_bb[-1]:
-                if not lower_middle_alert_triggered:
-                    send_line_alert( f"{symbol}, Lowest candle price: {low_prices[-1]}, timeframe: {timeframe}, hit middle BB")
-                    lower_middle_alert_triggered = True
-            else:
-                # reset the upper alert triggered flag
-                lower_middle_alert_triggered = False
+#            if low_prices[-1] < middle_bb[-1]:
+#                if not lower_middle_alert_triggered:
+#                    send_line_alert( f"{symbol}, Lowest candle price: {low_prices[-1]}, timeframe: {timeframe}, hit middle BB")
+#                    lower_middle_alert_triggered = True
+#            else:
+#                # reset the upper alert triggered flag
+#                lower_middle_alert_triggered = False
 
             if low_prices[-1] < lower_bb[-1]:               
                 if not lower_alert_triggered:
